7a9832422744e2f573c668c5147efbaaf711df40
Luis Lopez <darkdestruktion@gmail.com>
2019-09-08 23:48:37 -0500

    Crear dos ventanas

--- maquinaDeCasino/maquinaCasinoImagen.py
+++ maquinaDeCasino/maquinaCasinoImagen.py
@@ -29,54 +29,81 @@ from tkinter import *
 from PIL import ImageTk, Image
 
 counter = 0
+def create_window():
+    window = Toplevel(root)
 
-def counter_label():
-    start = time.time()
-    img = ImageTk.PhotoImage(Image.open("michi1.png"))
-    img2 = ImageTk.PhotoImage(Image.open("michi2.png"))
-    img3 = ImageTk.PhotoImage(Image.open("michi3.png"))
+    window.geometry('500x500')
 
-    photos = [img, img2, img3]
-    photos2 = [img2, img3, img]
-    photos3 = [img3, img, img2]
+    rows = 0
 
-    label = Label(root, image=photos[counter])
-    label2 = Label(root, image=photos[counter])
-    label3 = Label(root, image=photos[counter])
+    while rows < 50:
+        window.rowconfigure(rows, weight=1)
+        window.columnconfigure(rows, weight=1)
+        rows += 1
 
-    label.grid(row=2, column=0)
-    label2.grid(row=2, column=2)
-    label3.grid(row=2, column=4)
+    def counter_label():
+        start = time.time()
+        img = ImageTk.PhotoImage(Image.open("michi1.png"))
+        img2 = ImageTk.PhotoImage(Image.open("michi2.png"))
+        img3 = ImageTk.PhotoImage(Image.open("michi3.png"))
 
-    def count():
-        global counter
-        counter += 1
+        photos = [img, img2, img3]
+        photos2 = [img2, img3, img]
+        photos3 = [img3, img, img2]
 
-        if counter == 3:
-            counter = 0
+        label = Label(window, image=photos[counter])
+        label2 = Label(window, image=photos[counter])
+        label3 = Label(window, image=photos[counter])
 
-        label.config(image=photos[counter])
-        label2.config(image=photos2[counter])
-        label3.config(image=photos3[counter])
+        label.grid(row=5, column=0)
+        label2.grid(row=5, column=2)
+        label3.grid(row=5, column=4)
 
-        if time.time() - start < 3:
-            label.after(200, count)
-        else:
-            randNum = random.randint(0,2)
-            randNum2 = random.randint(0,2)
-            randNum3 = random.randint(0,2)
+        def count():
+            global counter
+            counter += 1
 
-            label.config(image = photos[randNum])
-            label2.config(image = photos2[randNum2])
-            label3.config(image = photos3[randNum3])
+            if counter == 3:
+                counter = 0
 
-    count()
+            label.config(image=photos[counter])
+            label2.config(image=photos2[counter])
+            label3.config(image=photos3[counter])
+
+            if time.time() - start < 3:
+                label.after(200, count)
+            else:
+                randNum = random.randint(0, 2)
+                randNum2 = random.randint(0, 2)
+                randNum3 = random.randint(0, 2)
+
+                label.config(image=photos[randNum])
+                label2.config(image=photos2[randNum2])
+                label3.config(image=photos3[randNum3])
+
+        count()
+
+    Button(window, text="Jugar", command=counter_label).grid(row = 2, column = 10)
+    Button(window, text="salir", command=window.destroy).grid(row=40, column=4)
+
 
 
 root = Tk()
 
-button1 = Button(root, text="Jugar", command=counter_label)
+root.title("Maquina de casino")
+root.geometry("500x500")
+
+rows = 0
+
+while rows < 50:
+    root.rowconfigure(rows, weight=1)
+    root.columnconfigure(rows, weight=1)
+    rows += 1
+
+button1 = Button(root, text="Juego", command=create_window)
 
 button1.grid(row=0, column=2)
+
+Button(root, text = "Salir", command = root.destroy).grid(row =49, column = 48)
 
 root.mainloop()
